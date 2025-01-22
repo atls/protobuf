5bab9f8dc41eaa87c7aa0381d32dfad2701d9dfc
Andrew Ghostuhin <me@torinasakura.name>
2025-01-22 17:15:24 +0300

    feat(protobuf-rpc): init

---
+++ packages/protobuf-rpc/src/index.ts
@@ -0,0 +1,4 @@
+export * from './gen/tech/atls/rpc/v1alpha1/validation-error_pb.js'
+export * from './gen/tech/atls/rpc/v1alpha1/logical-error_pb.js'
+export * from './validation-error.utils.js'
+export * from './logical-error.utils.js'
---
+++ packages/protobuf-rpc/src/validation-error.utils.ts
@@ -0,0 +1,15 @@
+import type { MaybeConnectError } from './connect-error.types.js'
+
+import { ValidationError }        from './gen/tech/atls/rpc/v1alpha1/validation-error_pb.js'
+
+export const findValidationErrorDetails = (error: unknown): Array<ValidationError> => {
+  const { details } = (error || {}) as MaybeConnectError
+
+  if (Array.isArray(details)) {
+    return details
+      .filter((detail) => detail.type === ValidationError.typeName)
+      .map((detail) => ValidationError.fromBinary(detail.value))
+  }
+
+  return []
+}
